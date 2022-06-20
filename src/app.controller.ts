a4e695ce82ae54b9357026afaf1d4f00d2ee7031
Fernando Rodriguez <fernando.rdzcrz@gmail.com>
2022-06-20 13:05:50 -0500

    functional webhook

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,30 +6,46 @@ import {
   Query,
   HttpException,
   HttpStatus,
+  Inject,
 } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('SAMPLE_SERVICE') private readonly client: ClientProxy,
+  ) {}
+  async onApplicationBootstrap() {
+    try {
+      await this.client.connect();
+      console.log('rabbitmq conectado');
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+
   @Post()
   async getInfoMesagge(@Body() data): Promise<any> {
-    const message_data = data;
-    await this.appService.send(
-      message_data.To,
-      message_data.From,
-      'Hola ' +
-        message_data.ProfileName +
-        ' como estas\n dijiste:' +
-        message_data.Body,
-    );
-    console.log(message_data);
+    const messageData = data;
+    // await this.appService.send(
+    //   messageData.To,
+    //   messageData.From,
+    //   'Hola ' +
+    //     messageData.ProfileName +
+    //     ' como estas\n' +
+    //     messageData.Body,
+    // );
 
-    return 1375339116267912;
+    this.client.emit<any>('whatsapp_message_received', messageData);
+    console.log(data.Body);
+
+    return true;
   }
 
   @Get()
-  hello(@Query() query): any {
+  auth(@Query() query): any {
     const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
     // Parse params from the webhook verification request
     const mode = query['hub.mode'];
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ClientsModule.register([
+      {
+        name: 'SAMPLE_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBIT_URL],
+          queue: 'whatsapp.messages.dev',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
